26399f43c0e0f8e180c592237d07435dc4d37843
leonjmac <leonjmac@protonmail.ch>
2024-03-17 23:13:01 +0000

    Add Braintree helper files.

---
+++ src/helpers/BTGatewayClient.ts
@@ -0,0 +1,16 @@
+import braintree, { BraintreeGateway } from 'braintree'
+
+const gateway = new BraintreeGateway({
+  environment: braintree.Environment.Sandbox,
+  publicKey: `${process.env.BRAINTREE_PUBLIC_KEY}`,
+  privateKey: `${process.env.BRAINTREE_PRIVATE_KEY}`,
+  merchantId: `${process.env.BRAINTREE_MERCHANT_ID}`
+ })
+
+ const oauthGateway = new BraintreeGateway({
+  environment: braintree.Environment.Sandbox,
+  clientId: `${process.env.BRAINTREE_OAUTH_CLIENT_ID}`,
+  clientSecret: `${process.env.BRAINTREE_OAUTH_CLIENT_SECRET}`
+ })
+
+ export { gateway, oauthGateway }
---
+++ src/helpers/BTGraphQLClient.ts
@@ -0,0 +1,54 @@
+import axios from 'axios'
+import { AppLogger, AppLoggerLevel } from '../middlewares/app-logger'
+
+const authorization = Buffer.from(
+  `${process.env.BRAINTREE_PUBLIC_KEY}:${process.env.BRAINTREE_PRIVATE_KEY}`,
+  'utf-8'
+).toString('base64')
+
+const client = axios.create({
+  baseURL: 'https://api.sandbox.braintreegateway.com/graphql',
+  headers: {
+    Authorization: `Basic ${authorization}`,    
+    'Braintree-Version': '2022-02-22'
+  }
+})
+
+const performRequest = async (query: any, variables: any) => {
+  try {
+    const response = await client.post('', {
+      query,
+      variables
+    }, {
+      headers: {
+        accept: 'application/json',
+        'Content-Type': 'application/json'
+      }
+    })
+    if (response.data.errors) throw response.data.errors
+    return response.data
+  } catch (err) {
+    // @ts-ignore
+    AppLogger(AppLoggerLevel.ERROR, err.message)
+    throw err
+  }
+}
+
+const performChainedRequest = async (json: any[]) => {
+  try {
+    const response = await client.post('', json, {
+      headers: {
+        accept: 'application/vnd+braintree.graphql.batch.v0+json',
+        'Content-Type': 'application/vnd+braintree.graphql.batch.v0+json'
+      }
+    })
+    if (response.data.errors) throw response.data.errors
+    return response.data
+  } catch (err) {
+    // @ts-ignore
+    AppLogger(AppLoggerLevel.ERROR, err.message)
+    throw err
+  }
+}
+
+export { performRequest, performChainedRequest }
